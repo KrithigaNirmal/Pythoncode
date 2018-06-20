8fc86fd79f87416b5a639c6b48642e5c2d90ebc6
Krithiga Nirmal <enknk6@gmail.com>
2018-06-19 21:00:39 -0500

    initial commit

---
+++ Loop.py
@@ -0,0 +1,17 @@
+data = 1
+while data <= 10:
+    print(data)
+    data = data + 1
+
+
+data1 = [1,2,0,5,10]
+a = 1
+for x in data1:
+    if x == 0:
+        continue
+    a = a*x
+print('The Value of a is {}'.format(a))
+
+data2 = [1,2,0,5,10]
+
+search = 0
